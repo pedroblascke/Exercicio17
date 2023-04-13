754406622c6f5774eb8e06e2210f776cccf53417
Pedro Blascke <pedro-lucasm@hotmail.com>
2023-04-13 18:18:44 -0300

    Finalizações

--- src/App.java
+++ src/App.java
@@ -2,56 +2,73 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Exercício 17: "Programa para loja de tintas"
-
-        //Entradas: O programa deverá pedir o tamanho em m² da área a ser pintada
-        System.out.println("Entre com a metragem a ser pintada em m²");
+        //Entradas
+        // -> O programa deverá pedir o tamanho em metros quadrados da área a ser pintada.
         Scanner teclado = new Scanner(System.in);
-        double metragem = teclado.nextDouble();
-        teclado.close();
 
-        //Processamentos: 
-        // 1°: cobertura 1L por 6m² = 1L/6m²
+        System.out.print("Entre com a metragem a ser pintada em m2: ");
+        double metragem = teclado.nextDouble();
+
+        System.out.println("");
+
+        System.out.print("Entre com o nome do pintor: ");
+        String nome = teclado.next();
+
+        teclado.close();
+        //Processamento
+        // -> Cobertura 1 litro por 6 metros quadrados
         double rendimentoLitros = 6;
 
-        // 2°: Lata 18L = 80 reais
-        double precoLata = 80;
+        // -> Lata 18 litros = R$ 80,00
+        double precoLata = 80.00;
         double volumeLataLitros = 18;
 
-        // 3°: Galões 3,6L = 25 reais
-        double precoGalao = 25;
+        // -> Galões 3.6 litros = R$ 25,00
+        double precoGalao = 25.00;
         double volumeGalao = 3.6;
 
-        // 4°: Calcular quantas latas de 18L
-            // 1 -> 1 -> 6
-            // metragem / (6 * 18) = 
+        // Calcular qts latas de tinta
         double quantidadeLatas = metragem / (volumeLataLitros * rendimentoLitros);
-
-        // 5°: Calcular o valor total em latas
+        // Calcular o valor total em latas
         double valorTotalLatas = quantidadeLatas * precoLata;
 
-        // 6°: Calcular quantos galões de 3,6L
+        //Calcular qts galões de tinta
         double quantidadeGaloes = metragem / (volumeGalao * rendimentoLitros);
         double valorTotalGaloes = quantidadeGaloes * precoGalao;
 
         //Quantidade a pintar para mistura
-        double metragemMistura = metragem * 1.1; //1,1 = 10%
-        double quantidadeLatasMistura = metragemMistura
-        double quantidadeLataMisturaArredondada = Math.ceil(metragemMistura / (volumeLataLitros * rendimentoLitros)); //Serve para arredondar o valor PARA CIMA.
-        double quantidadeaSerConvertidaEmGaloes = quantidadeLatasMisturaArredondada 
+        double metragemMistura = metragem * 1.1;
+        double quantidadeLatasMistura = metragemMistura / (volumeLataLitros * rendimentoLitros);
+        double quantidadeLatasMisturaArredondada = Math.ceil(metragemMistura / (volumeLataLitros * rendimentoLitros));
+        double quantidadeDesperdicioLata = quantidadeLatasMisturaArredondada - quantidadeLatasMistura;
+        double quatidadeLitrosAConverterEmGaloes = (1 - quantidadeDesperdicioLata) * volumeLataLitros;
+        double quantidadeEmGaloesArredondada = Math.ceil(quatidadeLitrosAConverterEmGaloes/volumeGalao);
+        double quantidadeEmLataFinal = Math.floor(quantidadeLatasMistura);
 
-        // 7°: Calcular a melhor quantidade de latas e galões, considerando quantidades de latas/galões cheios e 10% de folga.
-            
-        
 
-        //Saídas: apresentar os resultados dos cálculos.
-        System.out.printf("A metragem a ser pintada é de %f metros quadrados", metragem);
+        // Calcular qts galões de 3,6 litros
+        // Calcular a melhor quantidade de latas e galões, considerando quantides de latas e galões cheios e 10% de folga. Arredondando para cima
+        //Saídas
+        // Apresentar os resultados dos cálculos
+        System.out.printf("A metragem a ser pintada por %s é de %f metros quadrados", nome, metragem);
         System.out.println("");
         System.out.printf("A quantidade de latas é %f e o valor total em latas é %f", quantidadeLatas, valorTotalLatas);
         System.out.println("");
         System.out.printf("A quantidade de galões é %f e o valor total em galões é %f", quantidadeGaloes, valorTotalGaloes);
+        System.out.println("");
         System.out.printf("A metragem para mistura é igual a %f", metragemMistura);
-        System.out.printf("A quantidade de latas para mistura é igual a %f", quantidadeLataMisturaArredondada);
-        
-    }   
+        System.out.println("");
+        System.out.printf("A quantidade de latas para mistura é igual a %f", quantidadeLatasMistura);
+        System.out.println("");
+        System.out.printf("A quantidade de latas para mistura arredondada é igual a %f", quantidadeLatasMisturaArredondada);
+        System.out.println("");
+        System.out.printf("A quantidade desperdício na lata %f", quantidadeDesperdicioLata);
+        System.out.println("");
+        System.out.printf("A quantidade em litros a ser usada em galões é %f", quatidadeLitrosAConverterEmGaloes);
+        System.out.println("");
+        System.out.printf("A quantidade arredondada em galões é %f", quantidadeEmGaloesArredondada);
+        System.out.println("");
+        System.out.printf("A quantidade final em latas é %f e em galões é %f", quantidadeEmLataFinal, quantidadeEmGaloesArredondada);
+    }
+
 }
